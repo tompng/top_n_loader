cd33967e142f4db4d0a177bd67e1adf6636b79fb
tompng <tomoyapenguin@gmail.com>
2022-07-01 03:55:43 +0900

    select primary key

--- lib/top_n_loader/sql_builder.rb
+++ lib/top_n_loader/sql_builder.rb
@@ -9,6 +9,7 @@ module TopNLoader::SQLBuilder
   end
 
   def self.top_n_association_sql(klass, target_klass, relation, limit:, order_mode:, order_key:)
+    limit = limit.to_i
     parent_table = klass.table_name
     joins = klass.joins relation.to_sym
     target_table = target_klass.table_name
@@ -19,8 +20,14 @@ module TopNLoader::SQLBuilder
     join_sql = joins.to_sql.match(/FROM.+/)[0]
     parent_primary_key = "#{qt parent_table}.#{q klass.primary_key}"
     target_order_key = "#{qt target_table}.#{q order_key}"
-    return top_aggregate_sql target_table, order_key, parent_primary_key, from_sql: join_sql, order_mode: order_mode if limit == 1 && !nullable
-
+    target_primary_key = "#{qt target_table}.#{q target_klass.primary_key}"
+    top_n_key, top_n_alias, t_join_cond = limit == 1 ? [
+      target_primary_key, :top_n_primary_key, "#{target_primary_key} = top_n_primary_key"
+    ] : [
+      target_order_key, :top_n_order_key, compare_cond(target_order_key, order_mode, includes_nil: nullable)
+    ]
+    order_columns = limit == 1 ? [target_order_key, target_primary_key].uniq : [target_order_key]
+    order_cond = order_columns.map {|column| "#{column} #{order_mode.to_s.upcase}"}.join(', ')
     <<~SQL.squish
       SELECT #{qt target_table}.*, top_n_group_key
       #{join_sql}
@@ -28,38 +35,36 @@ module TopNLoader::SQLBuilder
       (
         SELECT T.#{q klass.primary_key} AS top_n_group_key,
         (
-          SELECT #{target_order_key}
+          SELECT #{top_n_key}
           #{join_sql}
           WHERE #{parent_primary_key} = T.#{q klass.primary_key}
-          ORDER BY #{target_order_key} #{order_mode.upcase}
-          LIMIT 1 OFFSET #{limit.to_i - 1}
-        ) AS last_value_of_key
+          ORDER BY #{order_cond}
+          LIMIT 1#{" OFFSET #{limit - 1}" if limit != 1}
+        ) AS #{top_n_alias}
         FROM #{qt parent_table} AS T WHERE T.#{q klass.primary_key} IN (?)
       ) T
-      ON #{parent_primary_key} = T.top_n_group_key AND #{compare_cond target_order_key, order_mode, includes_nil: nullable}
+      ON #{parent_primary_key} = T.top_n_group_key AND #{t_join_cond}
     SQL
   end
 
   def self.top_n_group_sql(klass:, group_column:, group_keys:, condition:, limit:, order_mode:, order_key:)
+    limit = limit.to_i
     table_name = klass.table_name
     sql = condition_sql klass, condition
     group_key_nullable = group_keys.include?(nil) && nullable_column?(klass, group_column)
     order_key_nullable = nullable_column? klass, order_key
-    if limit == 1 && !order_key_nullable
-      generated_sql = top_aggregate_sql(
-        table_name,
-        order_key,
-        "#{qt table_name}.#{q group_column}",
-        from_sql: "FROM #{qt table_name}",
-        condition_sql: sql,
-        includes_nil: group_key_nullable,
-        order_mode: order_mode
-      )
-      return [generated_sql, group_keys - [nil]]
-    end
     group_key_table = value_table(:T, :top_n_group_key, group_keys)
     table_order_key = "#{qt table_name}.#{q order_key}"
     join_cond = equals_cond "#{qt table_name}.#{q group_column}", includes_nil: group_key_nullable
+    table_primary_key = "#{qt table_name}.#{q klass.primary_key}"
+    top_n_key, top_n_alias, t_join_cond = limit == 1 ? [
+      table_primary_key, :top_n_primary_key, "#{table_primary_key} = top_n_primary_key"
+    ] : [
+      table_order_key, :top_n_order_key,
+      "#{compare_cond table_order_key, order_mode, includes_nil: order_key_nullable}#{" WHERE #{sql}" if sql}"
+    ]
+    order_columns = limit == 1 ? [table_order_key, table_primary_key].uniq : [table_order_key]
+    order_cond = order_columns.map {|column| "#{column} #{order_mode.to_s.upcase}"}.join(', ')
     <<~SQL.squish
       SELECT #{qt table_name}.*, top_n_group_key
       FROM #{qt table_name}
@@ -67,17 +72,14 @@ module TopNLoader::SQLBuilder
       (
         SELECT top_n_group_key,
         (
-          SELECT #{table_order_key} FROM #{qt table_name}
-          WHERE #{join_cond}
-          #{"AND #{sql}" if sql}
-          ORDER BY #{table_order_key} #{order_mode.to_s.upcase}
-          LIMIT 1 OFFSET #{limit.to_i - 1}
-        ) AS last_value_of_key
+          SELECT #{top_n_key} FROM #{qt table_name}
+          WHERE #{join_cond}#{" AND #{sql}" if sql}
+          ORDER BY #{order_cond}
+          LIMIT 1#{" OFFSET #{limit - 1}" if limit != 1}
+        ) AS #{top_n_alias}
         FROM #{group_key_table}
       ) T
-      ON #{join_cond}
-      AND #{compare_cond table_order_key, order_mode, includes_nil: order_key_nullable}
-      #{"WHERE #{sql}" if sql}
+      ON #{join_cond} AND #{t_join_cond}
     SQL
   end
 
@@ -86,7 +88,7 @@ module TopNLoader::SQLBuilder
     includes_nil ? "(#{cond} OR (#{column} IS NULL AND #{t_column} IS NULL))" : cond
   end
 
-  def self.compare_cond(column, order_mode, includes_nil:, t_column: 'T.last_value_of_key')
+  def self.compare_cond(column, order_mode, includes_nil:, t_column: 'T.top_n_order_key')
     if order_mode == :desc
       "(#{column} >= #{t_column} OR #{t_column} IS NULL)"
     elsif includes_nil
@@ -95,26 +97,6 @@ module TopNLoader::SQLBuilder
     else
       "(#{column} <= #{t_column} OR #{t_column} IS NULL)"
     end
-  end
-
-  def self.top_aggregate_sql(target_table, order_key, group_table_column, from_sql:, order_mode:, condition_sql: nil, includes_nil: false)
-    target_order_key = "#{qt target_table}.#{q order_key}"
-    order_func = order_mode == :asc ? :MIN : :MAX
-    <<~SQL.squish
-      SELECT #{qt target_table}.*, top_n_group_key
-      #{from_sql}
-      INNER JOIN
-      (
-        SELECT #{group_table_column} AS top_n_group_key, #{order_func}(#{target_order_key}) AS top_value_of_key
-        #{from_sql}
-        WHERE (#{group_table_column} IN (?)#{" OR #{group_table_column} IS NULL" if includes_nil})
-        #{"AND #{condition_sql}" if condition_sql}
-        GROUP BY #{group_table_column}
-      ) T
-      ON #{equals_cond group_table_column, includes_nil: includes_nil}
-      AND #{target_order_key} = T.top_value_of_key
-      #{"AND #{condition_sql}" if condition_sql}
-    SQL
   end
 
   def self.q(name)
@@ -167,12 +149,12 @@ module TopNLoader::SQLBuilder
     sql_binds = begin
       case value
       when NilClass
-        %(#{q key} IS NULL)
+        "#{q key} IS NULL"
       when Range
         if value.exclude_end?
-          [%(#{q key} >= ? AND #{q key} < ?), value.begin, value.end]
+          ["#{q key} >= ? AND #{q key} < ?", value.begin, value.end]
         else
-          [%(#{q key} BETWEEN ? AND ?), value.begin, value.end]
+          ["#{q key} BETWEEN ? AND ?", value.begin, value.end]
         end
       when Hash
         raise ArgumentError, '' unless value.keys == [:not]
@@ -180,12 +162,12 @@ module TopNLoader::SQLBuilder
       when Enumerable
         array = value.to_a
         if array.include? nil
-          [%((#{q key} IS NULL OR #{q key} IN (?))), array.reject(&:nil?)]
+          ["(#{q key} IS NULL OR #{q key} IN (?))", array.reject(&:nil?)]
         else
-          [%(#{q key} IN (?)), array]
+          ["#{q key} IN (?)", array]
         end
       else
-        [%(#{q key} = ?), value]
+        ["#{q key} = ?", value]
       end
     end
     sanitize_sql_array sql_binds
