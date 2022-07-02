b066cb1764fc4b2a79d7db832d6309a98607a171
tompng <tomoyapenguin@gmail.com>
2022-07-02 23:20:31 +0900

    changeable database config

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,6 @@ SimpleCov.start 'test_frameworks'
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "top_n_loader"
 require_relative './db'
-DB.migrate
+DB.migrate DB::DATABASE_CONFIG_SQLITE3
 DB.seed
 require "minitest/autorun"
